f1f6552d85fbf0d00a6252c3c9ab541d040cad10
Rahul33360 <109918304+Rahul33360@users.noreply.github.com>
2026-01-19 15:30:05 +0530

    day6 modifiend in same day2 app with models and learnt loops and iterators

---
+++ Day6/iterators.rb
@@ -0,0 +1,50 @@
+
+# each, select/reject, map, all, inject
+
+arr2 = [1,4,0,0,7,2]
+arr3 = arr2.select {|a| a<4} 
+puts arr3
+
+puts "Reject -> "
+# arr2 = [1,4,0,0,7,2]
+arr5 = arr2.reject {|a| a<4} 
+puts arr5
+
+
+# using bang operator -> !
+
+arr6 = [1,4,0,0,7,2]
+puts "Befor using bang Operator #{arr6}"
+arr7 = arr6.select! {|a| a<4}
+puts "After using bang Operator"
+puts "original arr #{arr6}"
+puts "new stored array #{arr7}"
+
+print "-------------\n"
+
+arr8 = [1,456,77,9,22,132,45]
+# puts arr8
+p arr8.all? {|n| n>5 }  # it return boolean value(false) if all the values will be greate than 5
+
+
+# below one it the opposite of all
+arr8 = [1,456,77,9,22,132,45]
+# puts arr8
+p arr8.any? {|n| n>5 }  # it return boolean value(true) if any the values will be greate than 5
+
+
+# map vs collect vs each
+# there is no difference in map and collect. they both are alisas.
+arr9 = [1,45,77,9,22,12,45]
+puts "map"
+arr10 = arr9.map {|i| i*2}
+p arr9
+p arr10
+puts "collect"
+arr11 = arr9.collect {|i| i*2}
+p arr9
+p arr11
+puts "each"
+arr12 = arr9.each {|i| i*2}
+p arr9
+p arr12
---
+++ Day6/loops.rb
@@ -0,0 +1,59 @@
+for i in 1..10 do
+    print "#{i} "   # run from 1 to 10 (inclusive 1 and 10)
+    # i+=1  
+end
+
+puts
+
+arr = [1,2,"adva",3,5,"rahul",7,9,0,94,31]
+
+for i in arr do
+    print "#{i} "
+end
+
+puts
+
+# while loop 
+j=0
+p "While loop"
+while j<10
+    print "#{j} "
+    j +=1
+end
+
+puts
+
+# util loop
+l=0
+until l>5 
+    print "until loop - #{l}, "
+    l+=1
+end
+
+puts
+
+5.times do
+    print "this is times iterator example\n"
+end
+
+
+# loop -> loop
+k=0
+loop do
+    print "loop #{k} - "
+    k+=1    
+break if k>4
+end
+
+puts
+
+# ye wala samjh me nhi aya
+
+for i in 1..10 
+    print "#{i}, "
+    redo if i>5
+    # break
+    # next if i>10
+end
+
+
