e57cefec45285f59d1f65aab6c60f15b39d75f0b
Rahul33360 <109918304+Rahul33360@users.noreply.github.com>
2026-01-21 22:25:30 +0530

    added flow base->Object->BasicObject

--- Day8/classes_checking.rb
+++ Day8/classes_checking.rb
@@ -1,29 +1,52 @@
 class Str
     def capitalize(value)
-        super
-        # p "hello #{value}"
+        # super
+        p "hello #{value}"
     end
 end
 
 s = Str.new
-# p s.to_s
-# p "ste".capitalize
-
-s.capitalize("raasgs")
+s.capitalize("raa")
 
 p Str.ancestors
 p s.respond_to?(:capitalize)
 
 
-# class Demo
-#     def object_id
-#         super
-        
-#     end
-# end
+class B 
+    #  from Object class 
+    def display
+    end
+    
+    # from Kernal module
+    def puts 
+    end
 
-# d=Demo.new
-# puts d.object_id
+    # from String class
+    def replace
+    end
 
+    def new
+    end
+
+    #Integer / Math class
+    def round
+    end
+
+    # Basic Object class
+    def  instance_exec
+    end
+
+end
+
+
+
+
+b= Base.new
+puts b.respond_to?(:puts)
+puts b.respond_to?(:display)
+puts b.respond_to?(:replace)
+puts b.respond_to?(:round)
+puts b.respond_to?(:instance_exec)
+puts b.respond_to?(:new)
 
 
